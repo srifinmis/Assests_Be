7fbc624a966258291cb1fd5c922703e39f056783
pbahadur <pappu.bahadur@srifincredit.com>
2025-08-02 11:18:02 +0530

    middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,14 +3,14 @@ const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
   const token = req.header("Authorization")?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "Access Denied" });
+  if (!token) return res.status(401).json({ message: "No token, unauthorized" });
 
   try {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified;
     next();
   } catch (err) {
-    res.status(403).json({ message: "Invalid Token" });
+    res.status(403).json({ message: "Invalid or expired token" });
   }
 };
 
